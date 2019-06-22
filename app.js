d55416410482422045a9bef0db9ba87a642400a1
Ripetaurus <lactic.nakul@gmail.com>
2019-06-22 19:04:11 +0530

    add home and semantic ui

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-     res.send('This is the root route');
+	res.render('home');
 });
 
 app.listen(8080, function() {
-     console.log('Server started on localhost:8080');
+	console.log('Server started on localhost:8080');
 });
-
-
